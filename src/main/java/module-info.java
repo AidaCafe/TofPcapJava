a65c2a901b12f53e9cbcede9491ade05b9b7c959
wkgcass <wkgcass@hotmail.com>
2023-06-15 11:10:33 +0800

    add maven publish plugin

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,9 @@ module net.cassite.tofpcap {
     requires org.pcap4j.core;
     requires org.pcap4j.packetfactory.statik;
     requires slf4j.api;
+
+    exports net.cassite.tofpcap;
+    exports net.cassite.tofpcap.messages;
+    exports net.cassite.tofpcap.parser;
+    exports net.cassite.tofpcap.util;
 }
